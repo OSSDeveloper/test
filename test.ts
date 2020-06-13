0cff10dccd232a53d0abee34a49195326c0526f2
Vijay <vijay@processbricks.com>
2020-06-14 01:32:51 +0530

    added jsdoc comments

--- test.ts
+++ test.ts
@@ -1,4 +1,9 @@
-
-export function add(a:number, b:number):number {
+/**
+ * 
+ * @param a {Number} any number
+ * @param b {Number} any number
+ * @returns {Number} sum of the two given numbers
+ */
+export function add(a: number, b: number): number {
     return a + b;
 }
